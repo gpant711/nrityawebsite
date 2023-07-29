2c3f4350d1c3c9cb5fede1ec4b8145220a15fdca
borakuldeep <36533806+borakuldeep@users.noreply.github.com>
2023-07-28 21:02:49 -0500

    1 file modified

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -11,12 +11,34 @@ const About: Component = () => {
         <div class={globalstyles.pagetext}>
           <h1>Welcome to NK Dance School</h1>
           <p>
-            Your gateway to the vibrant and captivating world of Indian dance
-            and culture right here in the US. We celebrate the rich tapestry of
-            culture and unleash the vibrant talents of our local community. We
-            take pride in nurturing a sense of community and fostering
-            cross-culture exchange.
+            Welcome to Nrityakalpna Dance School, where we celebrate the rich
+            tapestry of culture and unleash the vibrant talents of our local
+            community. At NK Dance School, we believe that dance is not only a
+            form of artistic expression but also a powerful medium for embracing
+            diversity, preserving traditions and fostering unity. Our dance
+            program not only emphasize technical excellence but also aim to
+            instill a profound appreciation for cultural diversity.
           </p>
+          <p>
+            We actively collaborate with local cultural organisations, community
+            centers and schools to organise performances, charity events that
+            bring community together. Wether you are a beginner or an
+            experienced dancer, a child or an adult, NK Dance School welcomes
+            you with open arms. Join us on this incredible journey of self -
+            discovery, cultural exploration and artistic expression. Unleash
+            your talent , connect with your roots and dance your way to a
+            brighter , more enriched future.
+          </p>
+          <h3>Courses offered:</h3>
+          <ul>
+            <li>Kathak Classes</li>
+            <li>Bollywood Beginners Classes</li>
+            <li>Bollywood Advanced Classes</li>
+            <li>Semi Classical Classes</li>
+            <li>Customized Choreography</li>
+            <li>Wedding Choreography</li>
+            <li>Cultural Event Choreography</li>
+          </ul>
         </div>
         <div class={styles.aboutimage}></div>
       </div>
